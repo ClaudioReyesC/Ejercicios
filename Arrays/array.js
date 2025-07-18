d4da548b165c3f0e7914a7cda718215074e38e3f
ClaudioReyesC <creyescanio@gmail.com>
2025-07-18 08:58:39 -0400

    arrays

---
+++ Arrays/array.js
@@ -0,0 +1,13 @@
+//Crear un arreglo con 5 frutas y mostrar su longitud.
+let frutas = ["manzana","pera","naranja","Frutilla","Sandia"]
+console.log(frutas)
+//Agregar una fruta al arreglo usando .push().
+frutas.push("UVA")
+console.log(frutas)
+//Eliminar la primera fruta usando .shift().
+frutas.shift(0)
+console.log(frutas)
+//Buscar si existe una fruta específica con .includes().
+frutas.includes("naranja")
+console.log(frutas.includes("naranja")
+//)
---
+++ Arrays/array_int.js
@@ -0,0 +1,16 @@
+//Crear una función que devuelva solo los elementos mayores a 10.
+
+let numeros =[10,5,1,20,3,2,25,15,11,7,8,9,4,6,66,100]
+console.log(numeros)
+
+function numeros_mayor(arr){
+    return arr.filter(num => num > 10);
+}
+console.log(numeros_mayor(numeros));
+
+//Sumar todos los elementos numéricos de un arreglo.
+//Crear una copia de un arreglo con .slice().
+//Ordenar un arreglo alfabéticamente.
+//Revertir un arreglo.
+//Usar .map() para multiplicar todos los números por 2.
+
